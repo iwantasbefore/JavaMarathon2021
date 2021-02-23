a2ba09dd01d72359a24039155a1acc9b1d276f5a
Yury Ermakov <iwantasbefore@gmail.com>
2021-02-23 19:18:28 +0300

    complete day1 tasks

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 10 ; i++) {
+            System.out.print("JAVA ");
+        }
 
     }
 }
--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            System.out.println("JAVA");
+            a = a + 1;
+        }
 
     }
 }
